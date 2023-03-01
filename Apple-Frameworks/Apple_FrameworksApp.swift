424b2f9f50b7cb1f7649ae394e3e16c1278cfb70
Junaed <junaed1995@gmail.com>
2023-03-01 09:26:41 +0600

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Junaed Muhammad Chowdhury on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
